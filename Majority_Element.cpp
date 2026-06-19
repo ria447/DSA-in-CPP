a24799384df6398db914147d2f2df2991be86a7e
Ria Mehta <rmehta4407@gmail.com>
2026-06-19 17:58:26 +0530

    Modify majorityElement
    Change the parameter type of majorityElement from 'vector<int>&' to 'vector<int>'.

--- Majority_Element.cpp
+++ Majority_Element.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int majorityElement(vector<int>& nums) 
+int majorityElement(vector<int> nums) 
 {
     // solving with the help of Moore's Voting Algorithm
   
