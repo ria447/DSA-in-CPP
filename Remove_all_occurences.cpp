1be4bb9b32aa340f316821c246698813106c8c49
Ria Mehta <rmehta4407@gmail.com>
2026-07-03 13:45:38 +0530

    Implement function to remove all occurrences of a substring

---
+++ Remove_all_occurences.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeOccurrences(string s, string part) 
+{
+    while(s.length() > 0 && s.find(part) < s.length())
+    {
+        s.erase(s.find(part), part.length());
+    }
+    return s;
+}
+
+int main()
+{
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    string output = removeOccurrences(string s, string part)
+    cout << output << endl;
+    return 0;
+}
