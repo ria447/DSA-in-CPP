e87866b2af41b7e84356ec0f4fcb89ebf6fb5555
Ria Mehta <rmehta4407@gmail.com>
2026-07-05 19:32:22 +0530

    Implement second approach for max profit calculation
    Added a second approach to calculate maximum profit from stock prices using a different algorithm.

--- Best_time_to_Buy_Stock.cpp
+++ Best_time_to_Buy_Stock.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//1st Approach
+
 int maxProfit(vector<int> prices) 
 {
       int MaxProfit = 0, BestBuy = prices[0];
@@ -18,6 +20,38 @@ int maxProfit(vector<int> prices)
       }
 
       return MaxProfit;
+}
+
+int main()
+{
+  vec<int> prices = {7,1,5,3,6,4};
+
+  int answer = maxProfit(prices);
+  return answer;
+}
+
+
+//2nd Approach
+
+int maxProfit(vector<int>& prices) 
+{
+      int best_buy = prices[0], max_profit = INT_MIN, profit;
+
+      if (prices.size() <= 1)
+            return 0;
+      else
+      {
+            for(int i = 1; i < prices.size(); i++)
+            {
+                best_buy = min(best_buy, prices[i]);
+                profit = prices[i] - best_buy;
+                max_profit = max(max_profit, profit);
+
+            }   
+      }
+        
+      return max_profit;
+    
 }
 
 int main()
