5474710c83bbf971e0b464b87d13a1a6a4c60e80
Ria Mehta <rmehta4407@gmail.com>
2026-07-21 13:59:56 +0530

    Add Fibonacci_sum.cpp with recursive function
    Implement Fibonacci function using recursion.

---
+++ Fibonacci_sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+//using recursion
+
+int fib(int n) 
+{
+    if(n == 0 || n == 1)
+    {
+       return n;
+    }   
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n = 4;
+    cout << fib(n) << endl;
+    return 0;
+}
+
+// output : 3
